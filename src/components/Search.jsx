4e42949373c9436cf36c6764015ea44ffe8e4f92
Yienbouaba Yarga <yarga_h@hotmail.com>
2025-08-30 13:14:49 +0200

    new search button added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -11,6 +11,13 @@ const Search = ({ searchTerm, setSearchTerm }) => {
                     placeholder='Search through thousands of movies'
                     value={searchTerm}
                     onChange={(event) => setSearchTerm(event.target.value)} />
+                    <button type="button" class="py-2 px-5 me-2 text-sm font-medium button_init
+                     text-gray-900 focus:outline-none
+                     bg-white rounded-lg border
+                     border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100
+                     dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white
+                     dark:hover:bg-gray-700"
+                     onClick={() => setSearchTerm('')}>new search</button>
             </div>
         </div>
     )
